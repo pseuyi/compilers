47c998e50012593296266b6a49d318ed78958119
pseuyi <pseuyi@gmail.com>
2019-11-30 21:11:07 -0500

    add exercise from category theory for programmers

---
+++ category-theory/4.4.ts
@@ -0,0 +1,29 @@
+// construct a kleisli category for partial functions
+
+/* 
+partial fns
+const root = (x) = Math.sqrt(x)
+const reciprocal = (x) => 1/x
+
+const compose = (f, g) => {
+  return (x) => {
+    g(f(x))    
+  }
+}
+*/
+
+interface Optional<A> {
+  value?: A,
+  isValid: boolean
+}
+
+// embelished fns
+const safeRoot = (x: number): Optional<number> => x < 0 ? { value: undefined, isValid: false } : { value: Math.sqrt(x), isValid: true }
+const safeReciprocal = (x: number): Optional<number> => x <= 0 ? { value: undefined, isValid: false } : { value: 1/x, isValid: true }
+
+const compose = (f, g) => {
+  return (x) => {
+    const { value, isValid } = f(x);
+    return isValid ? g(value) : undefined;
+  }
+}
