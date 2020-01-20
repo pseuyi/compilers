f5b8a259000be48d1d8f43d65a7936ee63b5e12b
pseuyi <pseuyi@gmail.com>
2020-01-20 17:27:35 -0500

    fix lesson 8, add lesson 9

--- get-programming-with-haskell/lesson-8/exercises.hs
+++ get-programming-with-haskell/lesson-8/exercises.hs
@@ -1,12 +1,13 @@
-myDrop n (x:xs) = myDrop (n - 1) xs
+mDrop n (x:xs) = myDrop (n - 1) xs
+
 myDrop 0 list = list
 
 myLength [] = 0
 myLength (x:xs) = 1 + myLength xs
 
-reverse [] = []
-reverse [x] = [x]
-reverse (x:xs) = reverse xs ++ x
+myReverse [] = []
+myReverse [x] = [x]
+myReverse (x:xs) = myReverse (xs ++ [x])
 
-fastFib n1 _ 0 = n1
-fastFib n1 n2 counter = fastFib (n1 + n2) n1 counter
+fastFib 1 n1 _ = n1
+fastFib n n1 n2 = fastFib (n - 1) (n1 + n2) n1
