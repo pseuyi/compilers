7b22d54b55039472873cf90a998ada4f916ae3f6
pseuyi <pseuyi@gmail.com>
2020-01-21 14:40:48 -0500

    finish capstone

--- get-programming-with-haskell/lesson-10/cup.hs
+++ get-programming-with-haskell/lesson-10/cup.hs
@@ -12,4 +12,36 @@ drink c oz =
     ozDiff = cOz - oz
 
 isEmpty c = getOz c == 0
+
 -- 10.2
+robot (name, attack, hp) = \msg -> msg (name, attack, hp)
+
+name (n, _, _) = n
+
+attack (_, a, _) = a
+
+hp (_, _, hp) = hp
+
+getName r = r name
+
+getAttack r = r attack
+
+getHP r = r hp
+
+setName r newName = r (\(n, a, h) -> r (newName, a, h))
+
+setAttack r newAttack = r (\(n, a, h) -> r (n, newAttack, h))
+
+setHP r newHP = r (\(n, a, h) -> r (n, a, newHP))
+
+printRobot r =
+  r (\(n, a, h) -> n ++ " attack:" ++ (show a) ++ " hp:" ++ (show h))
+
+damage r atk = r (\(n, a, h) -> r (n, a, h - atk))
+
+fight attacker defender = damage defender attack
+  where
+    attack =
+      if getHP attacker > 10
+        then getAttack attacker
+        else 0
