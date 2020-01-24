23e92fbde99d1a487454cf451c7f8c20bdf0c8da
pseuyi <pseuyi@gmail.com>
2020-01-24 17:05:21 -0500

    add solution

--- get-programming-with-haskell/lesson-12/exercises.hs
+++ get-programming-with-haskell/lesson-12/exercises.hs
@@ -3,4 +3,11 @@ type PatientName = (String, String)
 type Age = Int
 
 type Height = Int
---patientInfo :: PatientName -> Age -> Height -> String
+
+patientInfo :: PatientName -> Age -> Height -> String
+patientInfo (fst, lst) age height =
+  nameText ++ " " ++ ageText ++ " " ++ heightText
+  where
+    nameText = lname ++ ", " ++ fname
+    ageText = "(" ++ show age ++ "yrs."
+    heightText = show height ++ "in.)"
