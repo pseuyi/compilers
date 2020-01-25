c4528fedab1a96fc8b544f087306d52c722d54ca
pseuyi <pseuyi@gmail.com>
2020-01-25 14:18:49 -0500

    finish lesson 12

--- get-programming-with-haskell/lesson-12/exercises.hs
+++ get-programming-with-haskell/lesson-12/exercises.hs
@@ -11,3 +11,47 @@ patientInfo (fst, lst) age height =
     nameText = lname ++ ", " ++ fname
     ageText = "(" ++ show age ++ "yrs."
     heightText = show height ++ "in.)"
+
+data Sex
+  = Male
+  | Female
+
+type Weight = Int
+
+data BloodType =
+  BloodType ABOType RhType
+
+data ABOType
+  = A
+  | B
+  | AB
+  | O
+
+data RhType
+  = Pos
+  | Neg
+
+data Patient =
+  PatientName Sex Age Height Weight BloodType
+
+janeESmith :: Patient
+janeESmith = Patient (Jane Elizabeth, Smith) Male 30 74 200 (BloodType AB Pos)
+
+-- record
+data Patient =
+  Patient
+    { name :: PatientName
+    , sex :: Sex
+    , age :: Age
+    , height :: Height
+    , weight :: Weight
+    , bloodType :: BloodType
+    } -- name jackieSmith
+
+-- q12.1
+canDonate :: Patient -> Patient -> Bool
+canDonate a b = canDonateTo (bloodType a) (bloodType b)
+
+--q12.2
+patientSummary :: Patient -> String
+patientSummary p = "Sex: " ++ show (sex patient) ++ "\n"
