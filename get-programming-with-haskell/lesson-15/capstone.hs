317f7908af294c589e133ca23b76fab8ca4385fa
pseuyi <pseuyi@gmail.com>
2020-01-31 23:27:26 -0500

    start lesson 15

---
+++ get-programming-with-haskell/lesson-15/capstone.hs
@@ -0,0 +1,13 @@
+data FourLetterAlphabet
+  = L1
+  | L2
+  | L3
+  | L4
+  deriving (Show, Enum, Bounded)
+
+rotN :: (Bounded a, Enum a) => Int -> a -> a
+rotN alphabetSize c = toEnum rotation
+  where
+    halfAlphabet = n `div` 2
+    offset = fromEnum c + halfAlphabet
+    rotation = offset `mod` alphabetSize
