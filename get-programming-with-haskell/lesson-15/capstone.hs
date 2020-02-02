410d77237ed159fec7391abdbd45341adedac0e4
pseuyi <pseuyi@gmail.com>
2020-02-01 22:06:17 -0500

    fix

--- get-programming-with-haskell/lesson-15/capstone.hs
+++ get-programming-with-haskell/lesson-15/capstone.hs
@@ -6,8 +6,14 @@ data FourLetterAlphabet
   deriving (Show, Enum, Bounded)
 
 rotN :: (Bounded a, Enum a) => Int -> a -> a
-rotN alphabetSize c = toEnum rotation
+rotN n c = toEnum rotation
   where
     halfAlphabet = n `div` 2
     offset = fromEnum c + halfAlphabet
-    rotation = offset `mod` alphabetSize
+    rotation = offset `mod` n
+
+fourLetterAlphabetEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterEncoder vals = map rot 41 vals
+  where
+    alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+    rot41 = rotN alphaSize
