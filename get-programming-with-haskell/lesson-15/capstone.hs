0f217b165730dc9883ada14f46d9611aa807caff
pseuyi <pseuyi@gmail.com>
2020-02-01 22:43:55 -0500

    finish 15.4

--- get-programming-with-haskell/lesson-15/capstone.hs
+++ get-programming-with-haskell/lesson-15/capstone.hs
@@ -28,3 +28,64 @@ rotNdecoder n c = toEnum rotation
         then from Enum c + halfN
         else 1 + fromEnum c + halfN
     rotation = offset `mod` n
+
+xorBool :: Bool -> Bool -> Bool
+xorBool a b = (a || b) && (not (a && b))
+
+xorPair :: (Bool, Bool) -> Bool
+xorPair (v1, v2) = xorBool v1 v2
+
+xor :: [Bool] -> [Bool] -> [Bool]
+xor list1 list2 = map xorPair (zip list1 list2)
+
+type Bits = [Bool]
+
+intToBits' :: Int -> Bits
+intToBits' 0 = [False]
+intToBits' 1 = [True]
+intToBits' n =
+  if (remainder == 0)
+    then False : intToBits' nextVal
+    else True : intToBits' nextVal
+  where
+    remainder = n `mod` 2
+    nextVal = n `div` 2
+
+maxBits :: Int
+maxBits = length (intToBits' maxBound)
+
+intToBits :: Int -> Bits
+intToBits n = leadingFalses ++ reversedBits
+  where
+    reversedBits = reverse (intToBits' n)
+    missingBits = maxBits - (length reversedBits)
+    leadingFalses = take missingBits (cycle [False])
+
+charToBits :: Char -> Bits
+charToBits char = intToBits (fromEnum char)
+
+bitsToInt :: Bits -> Int
+bitsToInt bits = sum (map (\x -> 2 ^ (snd x)) trueLocations)
+  where
+    size = length bits
+    indices = [size - 1,size - 2 .. 0]
+    trueLocations = filter (\x -> fst x == True) (zip bits indices)
+
+bitsToChar :: Bits -> Char
+bitsToChar bits = toEnum (bitsToInt bits)
+
+-- pad bits w one time pad
+applyOTP' :: String -> String -> [Bits]
+applyOTP' pad plaintext =
+  map (\pair -> (fst pair) `xor` (snd pair)) (zip padBits plaintextBits)
+  where
+    padBits = map charToBits pad
+    plaintextBits = map charToBits plaintext
+
+applyOTP :: String -> String -> String
+applyOTP pad plaintext = map bitsToChar bitList
+  where
+    bitList = applyOTP' pad plaintext
+
+encoderDecoder :: String -> String
+encoderDecoder = applyOTP myOTP
