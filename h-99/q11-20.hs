20e126f1290cabc848df4b041f9078d2e7706e2b
pseuyi <pseuyi@gmail.com>
2020-02-06 18:20:06 -0500

    finish problem 15

--- h-99/q11-20.hs
+++ h-99/q11-20.hs
@@ -2,3 +2,8 @@
 dupli :: [a] -> [a]
 dupli [] = []
 dupli (x:xs) = [x] ++ [x] ++ dupli xs
+
+-- problem 15
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli list n = concatMap (\v -> map (\n -> v) [1 .. n]) list
