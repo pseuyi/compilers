0c7a86542046c464391507ebbe5dba78ee873bf4
pseuyi <pseuyi@gmail.com>
2020-02-06 23:46:22 -0500

    add lesson 17 quickchecks

--- get-programming-with-haskell/lesson-17/exercises.hs
+++ get-programming-with-haskell/lesson-17/exercises.hs
@@ -1,3 +1,9 @@
+import Data.Semigroup
+
 --17.1
 myAny :: (a -> Bool) -> [a] -> Bool
 myAny testFn = (foldl (||) False) . map testFn
+--17.2
+--(/) can return a non Int as a result so it cannot be a semigroup
+--17.3
+--(+) addition on Integers is associative
