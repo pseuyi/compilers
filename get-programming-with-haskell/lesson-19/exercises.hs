873044e13669ff2e77d3c0f194e5e085d1765217
pseuyi <pseuyi@gmail.com>
2020-02-28 10:52:59 -0500

    finish 19 qc

--- get-programming-with-haskell/lesson-19/exercises.hs
+++ get-programming-with-haskell/lesson-19/exercises.hs
@@ -26,3 +26,44 @@ numOrZero :: Maybe Int -> Int
 numOrZero
   | n == Nothing = 0
   | n == Just n = n
+
+-- 19.3
+data Container
+  = Vat Organ
+  | Cooler Organ
+  | Bag Organ
+
+data Location
+  = Lab
+  | Kitchen
+  | Bathroom
+  deriving (Show)
+
+organToContainer :: Organ -> Container
+organToContainer Brain = Vat Brain
+organToContainer Heart = Cooler Heart
+organToContainer organ = Bag organ
+
+placeInLocation :: Container -> (Location, Container)
+placeInLocation (Vat a) = (Lab, Vat a)
+placeInLocation (Cooler a) = (Lab, Cooler a)
+placeInLocation (Bag a) = (Kitchen, Bag a)
+
+process :: Organ -> (Location, Container)
+process organ = placeInLocation (organToContainer organ)
+
+report :: Maybe (Location, Container) -> String
+report (Just (location, container)) =
+  show container ++ " in the " ++ show location
+report Nothing = "error, organ not found"
+
+processRequest :: Int -> Map.Map Int Organ -> String
+processRequest id catalog = processAndReport organ
+  where
+    organ = Map.lookup id catalog
+
+processAndReport :: (Maybe Organ) -> String
+processAndReport (Just organ) = report (process organ)
+processAndReport Nothing = report Nothing
+--q19.1
+--q19.2
