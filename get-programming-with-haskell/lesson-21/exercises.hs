97c0e1c896da02248acff8d7d80bbc28ea5a8b97
pseuyi <pseuyi@gmail.com>
2020-03-02 23:32:19 -0500

    finish lesson 21

--- get-programming-with-haskell/lesson-21/exercises.hs
+++ get-programming-with-haskell/lesson-21/exercises.hs
@@ -43,3 +43,46 @@ main = do
   let pizza2 = (read size2, read cost2)
   let betterPizza = comparePizzas pizza1 pizza2
   putStrLn (describePizza betterPizza)
+
+-- Maybe Monad example
+costData :: Map.Map Int Double
+costData = Map.fromList [(1, 18.0), (2, 16.0)]
+
+sizeData :: Map.Map Int Double
+sizeData = Map.fromList [(1, 20.0), (2, 15.0)]
+
+maybeMain :: Maybe String
+maybeMain = do
+  size1 <- Map.lookup 1 sizeData
+  cost1 <- Map.lookup 1 costData
+  size2 <- Map.lookup 2 sizeData
+  cost2 <- Map.lookup 2 costData
+  let pizza1 = (size1, cost1)
+  let pizza2 = (size2, cost2)
+  let betterPizza = comparePizzas pizza1 pizza2
+  return (describePizza betterPizza)
+
+--q21.1
+helloPerson :: String -> String
+helloPerson name = "Hello" ++ " " ++ name ++ "!"
+
+nameData :: Map.Map Int String
+nameData = Map.fromList [(1, "wworld")]
+
+maybeMain2 :: Maybe String
+maybeMain2 = do
+  putStrLn "Hello! What's your name?"
+  name <- Map.lookup 1 nameData
+  let statement = helloPerson name
+  putStrLn statement
+
+--q21.2
+
+fastFib 1 n1 _ = n1
+fastFib n n1 n2 = fastFib (n - 1) (n1 + n2) n1
+
+mainFib :: IO ()
+mainFib ::
+  n <- getLine
+  let nth = fastFib n 1 1
+  putStrLn "the nth fib num is: " ++ nth
