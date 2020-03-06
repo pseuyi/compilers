469563e9e9a6f044bca727b4ad855a3ff888b6de
pseuyi <pseuyi@gmail.com>
2020-03-06 14:16:36 -0500

    add quickcheck solutions

--- get-programming-with-haskell/lesson-23/exercises.hs
+++ get-programming-with-haskell/lesson-23/exercises.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- w/ language pragma ^ above
-import qualified Data.Text as Text
+import qualified Data.Text as T
 
 firstWord :: String
 firstWord = "pessimism"
@@ -21,3 +21,19 @@ aWord = "Cheese"
 main :: IO ()
 main = do
   print aWord
+
+-- 23.2
+-- {-# LANGUAGE TemplateHaskell #-}
+-- 23.3
+sampleInput :: T.Text
+sampleInput = "this\nis\ninput"
+
+myTLines :: T.Text -> [T.Text]
+myTLines t = T.splitOn break t
+  where
+    break = "\n"
+
+myTUnlines :: [T.Text] -> T.Text
+myTUnlines t = T.intercalate break t
+  where
+    break = "\n"
