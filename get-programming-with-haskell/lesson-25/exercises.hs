f95cde2c7c78d92e3154ccf525093735c2bd4335
pseuyi <pseuyi@gmail.com>
2020-03-12 16:51:10 -0400

    add encoding

--- get-programming-with-haskell/lesson-25/exercises.hs
+++ get-programming-with-haskell/lesson-25/exercises.hs
@@ -16,3 +16,15 @@ bcInt = "6"
 
 intFromBc :: BC.ByteString -> Int
 intFromBc = read . BC.unpack
+
+-- text encoding stuff
+nagarjunaText :: T.Text
+nagarjunaText = "नागर्जुनॅ"
+
+nagarjunaB :: B.ByteString
+nagarjunaB = (BC.pack . T.unpack) nagarjunaText
+
+nagarjunaSafe :: B.ByteString
+nagarjunaSafe = E.encodeUtf8 nagarjunaText
+--TIO.putStrLn ((T.pack . BC.unpack) nagarjunaB) won't work
+--TIO.putStrLn (E.decodeUtf8 nagarjunaSafe) works
