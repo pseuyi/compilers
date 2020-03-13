77c0f2ad4ea77de287f62475dd35ee2b58e922ab
pseuyi <pseuyi@gmail.com>
2020-03-13 17:49:54 -0400

    init lesson 26

---
+++ get-programming-with-haskell/lesson-26/marc_to_html.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.ByteString as B
+import Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
+import qualified Data.Text.IO as TI
