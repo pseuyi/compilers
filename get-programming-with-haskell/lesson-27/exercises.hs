433dbed28c1ec42e3a7cddba642c091eb0c19adc
pseuyi <pseuyi@gmail.com>
2020-03-14 11:27:18 -0400

    finish 27.2

--- get-programming-with-haskell/lesson-27/exercises.hs
+++ get-programming-with-haskell/lesson-27/exercises.hs
@@ -2,3 +2,14 @@
 reverseMaybe :: Maybe String -> Maybe String
 reverseMaybe Nothing = Nothing
 reverseMaybe (Just str) = Just (reverse str)
+
+incMaybe :: Maybe Int -> Maybe Int
+incMaybe (Just n) = Just (n + 1)
+incMaybe Nothing = Nothing
+
+instance Functor Maybe where
+  fmap func (Just n) = Just (func n)
+  fmap func Nothing = Nothing
+
+fReverseMaybe :: Maybe String -> Maybe String
+fReeverseMaybe str = fmap reverse str
