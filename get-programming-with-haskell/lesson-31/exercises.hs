f43fa7e2b58b12a859d2fcffbc73b93d56b46262
pseuyi <pseuyi@gmail.com>
2020-03-28 14:48:25 -0400

    add 31.1

--- get-programming-with-haskell/lesson-31/exercises.hs
+++ get-programming-with-haskell/lesson-31/exercises.hs
@@ -7,3 +7,29 @@ nameStatement name = "Hello, " ++ name ++ "!"
 helloName :: IO ()
 helloName =
   askForName >> getLine >>= (\name -> return (nameStatement name)) >>= putStrLn
+
+helloNameDo :: IO ()
+helloNameDo = do
+  askForName
+  name <- getLine
+  putStrLn (nameStatement name)
+
+{--
+main :: IO ()
+main = do
+  name <- getLine
+  let statement = helloPerson name
+  putStrLn statement
+--}
+main :: IO ()
+main =
+  getLine >>= (\name -> (\statement -> putStrLn statement) (helloPerson name))
+
+--31.1
+echo :: IO ()
+echo = getLine >>= putStrLn
+
+echoDo :: IO ()
+echoDo = do
+  input <- getLine
+  putStrLn input
