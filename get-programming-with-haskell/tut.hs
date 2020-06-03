c59b96bcf10a660884777840585a5bad99ee7521
pseuyi <pseuyi@gmail.com>
2020-06-03 15:21:30 -0400

    add more examples

--- get-programming-with-haskell/tut.hs
+++ get-programming-with-haskell/tut.hs
@@ -24,4 +24,11 @@ everyOtherChar = ['A','C' .. 'Z']
 listTimes2 = [x * 2 | x <- [1 .. 10]]
 
 divBy9N13 = [x | x <- [1 .. 500], x `mod` 13 == 0, x `mod` 9 == 0]
+
 -- fizzBuzz
+--
+multTable = [[x * y | y <- [1 .. 10]] | x <- [1 .. 10]]
+
+firstChar :: String -> String
+firstChar [] = "empty string"
+firstChar all@(x:xs) = "the first letter in " ++ all ++ " is " ++ [x]
