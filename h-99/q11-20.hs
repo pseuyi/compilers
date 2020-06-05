50adbb86d90e5cb5c5fb24f70b7e4cd6729c6080
pseuyi <pseuyi@gmail.com>
2020-06-05 14:12:31 -0400

    try p16

--- h-99/q11-20.hs
+++ h-99/q11-20.hs
@@ -7,3 +7,15 @@ dupli (x:xs) = [x] ++ [x] ++ dupli xs
 repli :: [a] -> Int -> [a]
 repli [] _ = []
 repli list n = concatMap (\v -> map (\n -> v) [1 .. n]) list
+
+-- p16
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery ls 0 = ls
+dropEvery _ 1 = []
+dropEvery (x:xs) n = [x] ++ (_dE xs (n - 1) n)
+
+_dE :: [a] -> Int -> Int -> [a]
+_dE [] _ _ = []
+_dE (x:[]) 1 o = []
+_dE (x:xs) 1 o = dropEvery xs o
