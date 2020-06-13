f3529bb8bb3faf4274dde480173b55d15d48fd94
pseuyi <pseuyi@gmail.com>
2020-06-13 13:56:32 -0400

    add solution for q22

--- h-99/q21-28.hs
+++ h-99/q21-28.hs
@@ -3,3 +3,10 @@ insertAt :: a -> Int -> [a] -> [a]
 insertAt k 1 xs = [k] ++ xs
 insertAt k _ [] = [k]
 insertAt k n (x:xs) = [x] ++ (insertAt k (n - 1) xs)
+
+--q22
+range :: Int -> Int -> [Int]
+range a b
+  | b < a = [0]
+  | a == b = [a]
+  | otherwise = [a] ++ range (a + 1) b
