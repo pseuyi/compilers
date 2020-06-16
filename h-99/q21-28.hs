9582b1902e25be928229cbc2060513691a46be8b
pseuyi <pseuyi@gmail.com>
2020-06-15 22:10:43 -0400

    attempt rndSelect

--- h-99/q21-28.hs
+++ h-99/q21-28.hs
@@ -1,3 +1,6 @@
+import Control.Monad
+import System.Random
+
 --q21
 insertAt :: a -> Int -> [a] -> [a]
 insertAt k 1 xs = [k] ++ xs
@@ -10,3 +13,9 @@ range a b
   | b < a = [0]
   | a == b = [a]
   | otherwise = [a] ++ range (a + 1) b
+
+--q23
+rndSelect :: [a] -> Int -> IO [Int]
+rndSelect (x:xs) n = do
+  idxs <- replicateM n (randomRIO (1, (length xs)))
+  return idxs
